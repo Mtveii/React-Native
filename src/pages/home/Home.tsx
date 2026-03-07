5720ce2b1763f5d97581bb92ba0e25f8931a7967
-LeGasY- <maatveii567@gmail.com>
2026-03-07 11:55:33 +0200

    Add card

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,5 +4,22 @@ import HomeStyle from "./ui/HomeStyle";
 export default function Home() {
     return <View style={HomeStyle.pageContainer}>
         <Text style={HomeStyle.pageTitle}>Hello, World!</Text>
-    </View>;
+
+        <View style={HomeStyle.CardContainer}>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            </View>
+        <View style={HomeStyle.CardContainer}>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            </View>
+        <View style={HomeStyle.CardContainer}>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+            <View style={HomeStyle.CardItem}><View style={HomeStyle.CardImg}></View><Text style={HomeStyle.CardTitle}>#####</Text></View>
+        </View>
+
+    </View>;    
 }
