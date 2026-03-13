4f401fba1154f2234d51ad13c7ea2e82b5a87367
-LeGasY- <maatveii567@gmail.com>
2026-03-13 14:57:05 +0200

    calc commit 3

--- src/app/ui/AppContentStyle.ts
+++ src/app/ui/AppContentStyle.ts
@@ -1,66 +1,87 @@
 import { StyleSheet } from "react-native";
 import Colors from "../../features/config/Colors";
 
-const AppConstStyle = StyleSheet.create({
-    container:{
+const AppContentStyle = StyleSheet.create({
+    container: {
         flex: 1,
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
-        width: "100%"
+        width: "100%",
     },
-    text:{
+    topBar: {
+        backgroundColor: "#333",
+        height: 50.0,  // dip - density independent pixel
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    topBarBack: {
+        fontSize: 28.0,
         color: Colors.primaryTextColor,
+        fontWeight: 700,
+        marginLeft: 2.0,
+        // marginTop: -2.0,
     },
-    ButonBar:{
-        backgroundColor: "#333",
-        height: 60.0, 
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems:"center",
-    },
-    TopBar:{
-        backgroundColor: "#333",
-        height: 50.0, 
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    TopBarIcon: {
+    topBarIcon: {
         backgroundColor: "#bbb",
-        height: 42.0, 
-        width: 42.0,
+        height: 42.0,
         marginHorizontal: 10.0,
-    },
-    TopBarTitle:{
-        color:"#f1f1f1",
-        height: 42.0, 
-        display:"flex",
-        flexDirection:"row",
-        justifyContent:"space-evenly",
         width: 42.0,
+    },
+    topBarTitle: {
+        color: Colors.primaryTextColor,
+        flex: 1,
+        fontSize: 16.75,
+        fontWeight: 700,
         textAlign: "center",
     },
-    ButonBarIcon:{
-        backgroundColor: "#bbb",
-        height: 42.0, 
-         width: 42.0,
-        marginHorizontal: 10.0,
-
-    },
-    pegeWiget:{
+    pageWidget: {
         flex: 1,
-        justifyContent:"center",
+        justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#575757ff",
+    },
+    text: {
+        color: Colors.primaryTextColor,
+    },
+    bottomBar: {
+        backgroundColor: "#333",
+        height: 60.0,
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+    },
+    bottomBarIcon: {
+        tintColor: "#bbb",
+        height: 42.0,
+        width: 42.0,
+    },
+});
 
-        
-    }
+export default AppContentStyle;
 
-})
+/*
+Д.З. На головному віджеті сторінки розмітити "прелоадер" 
+товарів у вигляді напівпрозорих квадратів з плейсхолдерами
+назви під ними
 
-export default AppConstStyle;
+ *****     *****
+ *****     *****
+ *****     *****
+ -----     -----
+
+ *****     *****
+ *****     *****
+ *****     *****
+ -----     -----
+
+ *****     *****
+ *****     *****
+ *****     *****
+ -----     -----
+
+*/
